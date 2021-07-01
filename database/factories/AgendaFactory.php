1ca17deae67bf889cc7ccfe671efd15b5dea8f82
bernyanez <yanezbernardita@gmail.com>
2021-06-30 22:24:56 -0400

    Commit inicial

---
+++ database/factories/AgendaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\agenda;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AgendaFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = agenda::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
